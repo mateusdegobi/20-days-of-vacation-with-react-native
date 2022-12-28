73fe563caddba99e4fc6f9b01ea3938a0e14419e
mateus degobi <kim3dis@gmail.com>
2022-12-28 14:31:27 -0300

    fix: add await in promisse

--- src/pages/Weather/api/getWeather.ts
+++ src/pages/Weather/api/getWeather.ts
@@ -6,7 +6,7 @@ async function getWeather(lat: number, lon: number) {
   const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY_API_WEATHER}&units=metric&lang=pt_br`;
 
   const queryWeather = await axios.get(URL);
-  return queryWeather.data;
+  return await queryWeather.data;
 }
 
 export default getWeather;
