d0a1617f693f4d22c464725145b1ec68e42c0b71
mateus degobi <kim3dis@gmail.com>
2023-01-06 12:22:46 -0300

    chore: add heart icon

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,6 +1,12 @@
 import settings from "../assets/settings-sliders.png";
 import arrowLeft from "../assets/arrow-left.png";
+import heartActive from "../assets/heart-active.png";
+import heartInactive from "../assets/heart-inactive.png";
 
-export type NameIconsTypes = "settings" | "arrowLeft";
+export type NameIconsTypes =
+  | "settings"
+  | "arrowLeft"
+  | "heartActive"
+  | "heartInactive";
 
-export default { settings, arrowLeft };
+export default { settings, arrowLeft, heartActive, heartInactive };
