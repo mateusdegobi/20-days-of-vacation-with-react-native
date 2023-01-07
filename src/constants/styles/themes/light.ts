032b8db825496f0f25c4d19f7188ba0455be6403
mateus degobi <kim3dis@gmail.com>
2023-01-07 13:15:00 -0300

    chore: add const to colors

---
+++ src/constants/styles/themes/light.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from "styled-components/native";
+import * as Colors from "../colors";
+
+export const themeLight: DefaultTheme = {
+  colors: {
+    main: Colors.purple,
+    secondary: "",
+  },
+  font: {
+    "font-family": "",
+  },
+  text: {
+    h1: "",
+    h2: "",
+    h3: "",
+    paragraph: "",
+  },
+};
